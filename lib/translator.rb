84b2ffe8ea787041b645f449ac0f48ac55282a29
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-21 22:13:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,16 @@
 require "yaml"
 
 
-def load_library(data)
-emoticons = YAML.load_file('lib/emoticons.yml') 
- 
+def load_library(file_path)
+ #code goes here
+  emo = YAML.load_file("lib/emoticons.yml")
+  frank_emo = {}
+  emo.each do |name,arrays|
+      frank_emo[name] ||= {}
+      frank_emo[name][:english] = arrays[0]
+      frank_emo[name][:japanese] = arrays[1]
+  end
+frank_emo
 end
 
 def get_japanese_emoticon(file_path, eng_emo)
