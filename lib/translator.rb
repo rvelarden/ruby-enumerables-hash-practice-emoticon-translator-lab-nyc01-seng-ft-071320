0ddf06c025aa29e0330fe4177120d3ef1999dcb4
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-21 21:42:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,115 @@
 # require modules here
 
-def load_library
-  # code goes here
+def load_library(data)
+  {
+   :angel => {:english => "O:)"}, 
+             {:japanese => "☜(⌒▽⌒)☞"},
+ 
+ 
+ 
+   :angry =>
+             {:english => ">:("},
+             {:japanese => "ヽ(ｏ`皿′ｏ)ﾉ"},
+ 
+ 
+   :bored =>
+             {:english => ":O"},
+             {:japanese => "(ΘεΘ;)"},
+ 
+
+ 
+   :confused =>
+             {:english => "%)"},
+             {:japanese => "(゜.゜)"},
+ 
+
+ 
+   :embarrassed =>
+             {:english => ":$"},
+             {:japanese => "(#^.^#)"},
+ 
+
+ 
+   :fish =>
+   { 
+     :english => "><>",
+     :japanese => ">゜))))彡"
+   },
+ 
+
+ 
+   :glasses =>
+   { 
+     :english => "8D",
+     :japanese => "(^0_0^)"
+   },
+ 
+ {
+   :grinning =>
+   { 
+     :english => "=D",
+     :japanese => "(￣ー￣)"
+   },
+ 
+
+ 
+   :happy =>
+   { 
+     :english => ":)",
+     :japanese => "(＾ｖ＾)"
+   },
+ 
+
+ 
+   :kiss =>
+   { 
+     :english => ":*",
+     :japanese => "(*^3^)/~☆"
+   },
+ 
+
+ 
+   :sad =>
+   { 
+     :english => ":'(",
+     :japanese => "(Ｔ▽Ｔ)"
+   },
+ 
+
+ 
+   :surprised =>
+   { 
+     :english => ":o",
+     :japanese => "o_O"
+   },
+ 
+
+ 
+   :wink =>
+   { 
+     :english =>  ";)",
+     :japanese => "(^_-)"
+   }
+ }
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, eng_emo)
   # code goes here
+  library = load_library('./lib/emoticons.yml')
+  library.each do |meaning, idioms|
+      return idioms[:japanese] if idioms[:english] == eng_emo
+         
+  end 
+
+  "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, jap_emo)
   # code goes here
-end
+  library = load_library('./lib/emoticons.yml')
+  library.each do |meaning, idioms|
+    return meaning if idioms[:japanese] == jap_emo
+    
+  end
+    "Sorry, that emoticon was not found"
+end 
